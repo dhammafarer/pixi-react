34538f1db64342646d62bbed9920b3ef05ba645c
Pawel Lisewski <pawel.lisewski@gmail.com>
2017-07-31 17:48:35 +0800

    create pixi-helpers

--- src/Graphic.js
+++ src/Graphic.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import * as shapes from './lib/shapes.js';
 import isometricGrid from './lib/isometric-grid.js';
 import { Stage, Graphics } from 'react-pixi';
+import { drawShape } from './helpers/pixi-helpers.js';
 
 class Graphic extends React.Component {
   constructor (props) {
@@ -22,18 +23,7 @@ class Graphic extends React.Component {
 
     let graphics = this.graphics;
     shapes.grid(grid)
-      .forEach(t => this.drawShape(graphics, t));
-  }
-
-  drawShape (tool, points) {
-    tool.beginFill(0xFFFFFF);
-    tool.lineStyle(2, 0x333333, 1);
-    let head = points[0];
-    let tail = points.slice(1);
-    tool.moveTo(head.x, head.y);
-    tail.forEach(p => tool.lineTo(p.x, p.y));
-    tool.endFill();
-    tool.closePath();
+      .forEach(t => drawShape(graphics, t));
   }
 
   render () {
